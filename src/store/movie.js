fdcba622193771dddab2c12480343f4225ddff65
jeunseon <euns420@naver.com>
2024-03-24 18:15:14 +0900

    state 데이터 갱신 Object.keys().forEach() context.commit()

--- src/store/movie.js
+++ src/store/movie.js
@@ -5,13 +5,21 @@ export default {
     namespaced: true,
     // data!
     state: () => ({
-        movies: []
+        movies: [],
+        message: '',
+        loading: false
     }),
     // computed!
     getters: {},
     // methods!
     // 변이
     mutations: {
+        updateState(state, payload) {
+            // ['movies', 'message', 'loading']
+            Object.keys(payload).forEach(key => {
+                state[key] = payload[key]
+            })
+        },
         resetMovies(state) {
             state.movies = []
         }
@@ -21,9 +29,14 @@ export default {
         async searchMovies(context, payload) {
             const {title, type, number, year } = payload
             const OMDB_API_KEY = '7035c60c'
-            
+
             const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
-            console.log(res)
+            const {Search, totalResults } = res.data
+            context.commit('updateState', {
+                movies: Search,
+                message: 'Hello world!!',
+                loading: true
+            })
         }
     }
 }
