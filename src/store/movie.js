3f102fad126ebefa4d2877268be5b1093dfd26c2
jeunseon <euns420@naver.com>
2024-03-24 17:06:18 +0900

    store movieactions Search 컴포넌트 apply 메소드를 store의 actions로 옮김  .dispatch() 메소드를 사용

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export default {
     // module!
     namespaced: true,
@@ -6,11 +8,7 @@ export default {
         movies: []
     }),
     // computed!
-    getters: {
-        movieIds(state) {
-            return state.movies.map(m => m.imdbID)
-        }
-    },
+    getters: {},
     // methods!
     // 변이
     mutations: {
@@ -20,8 +18,12 @@ export default {
     },
     // 비동기
     actions: {
-        searchMovies({ state, commit }) {
+        async searchMovies(context, payload) {
+            const {title, type, number, year } = payload
+            const OMDB_API_KEY = '7035c60c'
             
+            const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+            console.log(res)
         }
     }
 }
