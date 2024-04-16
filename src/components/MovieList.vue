8dfcf651aca563c82d7bb23d460bd6152832c757
jeunseon <euns420@naver.com>
2024-04-16 17:01:50 +0900

    movie list scss로 영화 이미지 배치

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -4,16 +4,19 @@
             <div class="message">
                 {{ message }}
             </div>
-            <MovieItem 
+            <div class="movies">
+                <MovieItem 
                 v-for="movie in movies"
                 :key="movie.imdbID" 
                 :movie="movie" />
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import MovieItem from '~/components/MovieItem';
+import MovieItem from '~/components/MovieItem'
+
 export default {
     components: {
         MovieItem
@@ -28,3 +31,13 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+    .container{
+        .movies{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+        }
+    }
+</style>
